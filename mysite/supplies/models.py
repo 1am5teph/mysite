315a88203ee47b0bea0bd3d1b31b8cf08b235229
1am5teph <marson.stephanie@gmail.com>
2017-07-30 17:00:19 -0700

    Added supplies model.

---
+++ mysite/supplies/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Item(models.Model):
+  item_name = models.CharField(max_length=100)
+  description = models.TextField()
+
+  def __str__(self):
+      return self.item_name
+
+class Vendor(models.Model):
+  vendor_name = models.CharField(max_length=100)
+
+  def _str_(self):
+      return self.vendor_name
+
+class Customer(models.Model):
+  customer_name = models.CharField(max_length=100)
+
+  def _str_(self):
+      return self.customer_name
