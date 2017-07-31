9eb9a35b4352b924d6c6e69e3fe95ecdb2e6b88a
1am5teph <marson.stephanie@gmail.com>
2017-07-30 19:18:01 -0700

    added item_list view

--- mysite/supplies/models.py
+++ mysite/supplies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Item(models.Model):
   item_name = models.CharField(max_length=100)
-  description = models.TextField()
+  item_quantity = models.IntegerField()
 
   def __str__(self):
       return self.item_name
--- mysite/supplies/views.py
+++ mysite/supplies/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Item
+
+def item_list(request):
+    items = Item.objects.all()
+    #output = ', '.join([str(item) for item in items])
+    return render(request, 'supplies/item_list.html', {'items' : items})
